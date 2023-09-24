5747775d29f631df4955a6fad28551e40e141595
Manuel Valles <manuel.valles@beamery.com>
2023-09-24 12:49:28 +0100

    Web-standard API. Example Simple Routes

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 const server = Bun.serve({
     port: Bun.env.PORT || 3000,
     fetch(req){
-        return new Response('Hello Bun!')
-    }
+        const url = new URL(req.url);
+        if(url.pathname === '/') return new Response('Homepage!')
+        if(url.pathname === '/about') return new Response('About page!')
+        return new Response('Not found!', { status: 404 })
+    },
 })
 
 console.log(`Server running at ${server.port}`)
